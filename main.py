2b270e76809df5d63f23c75fe16b8d908ec0d0f4
ChengzhiLi <33775078+li-chengzhi@users.noreply.github.com>
2021-10-04 08:21:16 +0000

    First Prod version on MADNFTs channel, but it still misses some transactions and have some downtime.

--- main.py
+++ main.py
@@ -11,17 +11,22 @@ import asyncio
 my_secret = os.environ['TOKEN']
 ETHERSCAN_API_KEY = os.environ['ETHERSCAN_API_KEY']
 client = discord.Client()
-
+MINUTES = 2 #回看的频率，每MINUTE分钟
+#DC_CHANNEL_ID = 894187422290685963 #test server
+DC_CHANNEL_ID = 894169478177390612 #production on MADNFTs
 
 """对于所有的交易，都保存在里面，key是poet名字，value是历史的成交记录"""
 def storeTransactions(sales,db):
   # = 对每笔交易进行处理
   db_df = pd.DataFrame(db)
+  print('Currently, the dabatbase has data:', db_df)
+  print(db_df.shape)
   new_sales = [] #储存新的交易，不存在数据库中的
   for sale in sales:
     if 'payment_token' in sale.keys():
       #page或者诗人的名字
       poet = sale['asset']['name']
+      print('Found new sale on opeansea:',poet)
       #售价方式和金额
       payment_symbol = sale['payment_token']['symbol']
       payment_symbol_amt = scanEtherTransaction(sale)
@@ -58,6 +63,8 @@ def storeTransactions(sales,db):
       print('At time: {}, {} was sold! \n Sale Price: {} {}\n Buyer:{}, Seller:{}'.format(sale_time_bj,poet,payment_symbol,payment_symbol_amt,buyer,seller))
 
   new_sales_df = pd.DataFrame(new_sales)
+  print('Add new sales.., with shape..', new_sales_df)
+  print(new_sales_df.shape)
   updated_db = pd.concat([db_df, new_sales_df], axis=0).drop_duplicates()
   db = updated_db.to_dict()
   print('db is updated!')
@@ -65,9 +72,10 @@ def storeTransactions(sales,db):
   return updated_db
 
 """从opeansea上获得最新的交易，并放入数据库中"""
-def getTransactions(db,num=10): #todo:修改默认值
+def getTransactions(db,num=20): #todo:修改默认值
   now = datetime.now()
-  one_day_ago = now - timedelta(minutes=5)
+  #= 设置一个查询的范围
+  interval_start = now - timedelta(minutes=MINUTES)
   url = "https://api.opensea.io/api/v1/events"
   # OPEANSEA: LOSTPOETS
   # ref:https://docs.opensea.io/reference/retrieving-asset-events
@@ -76,13 +84,14 @@ def getTransactions(db,num=10): #todo:修改默认值
                 "offset":"0",
                 "limit": num, #可以调整 最多300
                 'event_type':'successful', #获取最新交易数据
-                'occurred_after': one_day_ago,
+                'occurred_after': interval_start,
                 'occurred_before': now}
   headers = {"Accept": "application/json"}
   response = requests.request("GET", url, headers=headers, params=querystring)
   #=将数据转换成JSON格式
   sales = response.json()['asset_events']
-
+  print('Found new sales on opensea:', len(sales))
+  print('Storing transactions....')
   db_df = storeTransactions(sales,db)
   return db_df
 
@@ -101,35 +110,36 @@ def scanEtherTransaction(sale):
   value = int(etherscan['input'][start:end],16)/1e18 #in Wei so convert
   return value
 
-# =开启后台发消息模式
+# =这是最重要的程序：自动从后台发消息
 async def my_background_task():
     await client.wait_until_ready()
-    #= todo: replace with production channel id
-    channel = client.get_channel(id=894169478177390612)
+    #= 这个决定了发布消息的channel
+    channel = client.get_channel(id=DC_CHANNEL_ID)
     await channel.send('I am a hard working bot.. Start working..')
     while not client.is_closed():
-        #获取过去5分钟的交易，并存入数据库中 todo:可以修改频率
+        #获取过去一段时间的交易，并存入数据库中 
         db_df = getTransactions(db)
         print('Transactions done!')
-        #过去2小时
-        deadline = datetime.now() - timedelta(minutes=2)
-        matched = db_df[db_df['sale_time']>=deadline]
-        print(deadline)
-        if matched.shape!= 0:
-          channel.send('Test sending feature: Vincent')
-          for idx, poet in matched.iterrows():
-            print('sending poet: ', poet['name'])
-            embedVar = discord.Embed(title=poet['name']+" sold!", 
-                                    description="Price: "+poet['price'], 
-                                    color=0x00ff00)
-            embedVar.add_field(name="Quantity", value=poet['quantity'], inline=False)
-            embedVar.add_field(name="Buyer", value=poet['buyer'], inline=True)
-            embedVar.add_field(name="Seller", value=poet['seller'], inline=True)
-            embedVar.add_field(name="Sale Time (Beijing Time)", value=poet['sale_time_bj'], inline=False)
-            embedVar.set_image(url=poet['image_url'])
-            await channel.send(embed=embedVar)
-        #await channel.send(embed=embedVar)
-        await asyncio.sleep(60) # task runs every 60 seconds
+        #过去5分钟的交易
+        deadline = datetime.now() - timedelta(minutes=MINUTES)
+        #=查看db_df是否有新的交易
+        #await channel.send('Smart bot found {} transactions during past {} minutes'.format(db_df.shape[0],MINUTES))
+        if db_df.shape[0] != 0: 
+          matched = db_df[db_df['sale_time']>=deadline].sort_values(by=['sale_time'])
+          #= 如果有新的交易，则发布到channel上
+          if matched.shape[0]!= 0:
+            for idx, poet in matched.iterrows():
+              print('sending poet: ', poet['name'])
+              embedVar = discord.Embed(title=poet['name']+" sold!", 
+                                      description="Price: "+poet['price'], 
+                                      color=0x00ff00)
+              embedVar.add_field(name="Quantity", value=poet['quantity'], inline=False)
+              embedVar.add_field(name="Buyer", value=poet['buyer'], inline=True)
+              embedVar.add_field(name="Seller", value=poet['seller'], inline=True)
+              embedVar.add_field(name="Sale Time (Beijing Time)", value=poet['sale_time_bj'], inline=False)
+              embedVar.set_image(url=poet['image_url'])
+              await channel.send(embed=embedVar)
+        await asyncio.sleep(MINUTES*60) # task runs every 60 seconds
 client.loop.create_task(my_background_task())
 
 
